89dd4f82c734f2f950b465553f3c994f73d8b762
Tong <m13302061411@163.com>
2019-04-03 16:35:52 +0800

    c

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ var config = {
     // path && publickPath
     output: {
         path        : __dirname + '/dist/',
-        publicPath  : WEBPACK_ENV === 'online' ? '//s.shendengnian.com/admin-fe/dist/' : '/dist/',
+        publicPath  : WEBPACK_ENV === 'online' ? '//s.marinatedegg.top:81/admin-fe/dist/' : '/dist/',
         filename    : 'js/[name].js'
     },
     resolve: {
